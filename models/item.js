432ecdd3e81062a0f982009e2c5ebf3fb93755df
ZManak <javifuer@gmail.com>
2023-03-07 12:45:33 +0100

    added/models

--- models/item.js
+++ models/item.js
@@ -1,4 +1,4 @@
-const { Sequelize, DataTypes } = require('sequelize');
+const {  Sequelize, DataTypes } = require('sequelize');
 const {db} = require('../pool/db');
 
 const Item = db.define("Item", {
---
+++ models/maker.js
@@ -0,0 +1,25 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const { db } = require('../pool/db');
+
+const Maker = db.define("Item", {
+    _id: {
+        type: DataTypes.UUID,
+        allowNull: false
+    },
+    makerName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    cif: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    address: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+}, {
+    timestamps: false
+});
+
+module.exports = Maker
