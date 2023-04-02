c53196fe6790ec73ab6cd1708efa72094b05cff8
ZManak <javifuer@gmail.com>
2023-04-02 21:27:24 +0200

    borders

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const cors = require("cors");
 const fetchRandomEquipment = require("./wip-fetch/item_dd.js");
 
-const itemRoutes = require("./routes/productsRoutes");
+const itemRoutes = require("/routes/productsRoutes");
 
 const app = express();
 const port = 3000;
@@ -14,12 +14,10 @@ app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
   //*Set static folder up in production
-  app.use(express.static("bazaar_client/build"));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "bazaar_client", "build", "index.html")
-    )
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 }
 
@@ -28,5 +26,5 @@ app.use("/api", itemRoutes);
 //fetchRandomEquipment().then(console.log);
 
 app.listen(port, () => {
-  console.log(`server running on http://localhost:${port}`);
+  console.log(`server running`);
 });
