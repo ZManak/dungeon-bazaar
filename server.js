d7c674be4cf7452a23c5474283a0ae8c3ec9d92e
ZManak <javifuer@gmail.com>
2023-04-03 07:57:10 +0200

    serve static

--- server.js
+++ server.js
@@ -12,18 +12,18 @@ const port = 3000;
 app.use(morgan("dev"));
 app.use(cors());
 
-/* if (process.env.NODE_ENV === "production") {
+app.use("/api", itemRoutes);
+
+//fetchRandomEquipment().then(console.log);
+
+if (process.env.NODE_ENV === "production") {
   //*Set static folder up in production
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
-} */
-
-app.use("/api", itemRoutes);
-
-//fetchRandomEquipment().then(console.log);
+}
 
 app.listen(port, () => {
   console.log(`server running`);
